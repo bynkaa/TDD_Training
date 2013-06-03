36c4c6598989a50ba372b754f880ebe292d8b3fc
Binh Tran <binhtran.vnu@gmail.com>
2013-06-03 13:52:58 +0700

    01

---
+++ TDDCalculator03/src/test/java/com/qsoft/examples/TestCalculator.java
@@ -0,0 +1,19 @@
+package com.qsoft.examples;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BinkaA
+ * Date: 6/3/13
+ * Time: 1:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestCalculator {
+    @Test
+    public void testEmptyString(){
+        assertEquals(0,"");
+    }
+}
