543ca38befbcbcd14d03fb0f97162e9eab0a4f51
Binh Tran <binhtran.vnu@gmail.com>
2013-06-04 13:50:57 +0700

    01

---
+++ TDDCalculatorDay04/src/test/java/com/qsoft/java/TestCalculator.java
@@ -0,0 +1,11 @@
+package com.qsoft.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BinkaA
+ * Date: 6/4/13
+ * Time: 1:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestCalculator {
+}
