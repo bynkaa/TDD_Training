ab4ef603210c8ed644685d29c07117ffffa656b0
Binh Tran <binhtran.vnu@gmail.com>
2013-06-04 13:57:05 +0700

    01

---
+++ TDDCalculatorDay04/src/main/java/com/qsoft/java/Calculator.java
@@ -0,0 +1,26 @@
+package com.qsoft.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: BinkaA
+ * Date: 6/4/13
+ * Time: 1:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Calculator {
+    public static int add(String s) {
+
+        if (s.isEmpty())
+            return 0;
+        else if (s.contains(",")){
+            String[] listNumbers = s.split(",");
+            int sum  = 0;
+            for (String number : listNumbers){
+                sum += Integer.parseInt(number);
+            }
+            return sum;
+        }
+        return -1;
+
+    }
+}
--- TDDCalculatorDay04/src/test/java/com/qsoft/java/TestCalculator.java
+++ TDDCalculatorDay04/src/test/java/com/qsoft/java/TestCalculator.java
@@ -1,5 +1,9 @@
 package com.qsoft.java;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created with IntelliJ IDEA.
  * User: BinkaA
@@ -8,4 +12,16 @@ package com.qsoft.java;
  * To change this template use File | Settings | File Templates.
  */
 public class TestCalculator {
+    @Test
+    public void testEmptyString(){
+       assertEquals(0, Calculator.add(""));
+    }
+    @Test
+    public void testSingerNumber(){
+        assertEquals(1,Calculator.add("1"));
+    }
+    @Test
+    public void testSimpleCommaDelimiter(){
+        assertEquals(3,Calculator.add("1,2"));
+    }
 }
