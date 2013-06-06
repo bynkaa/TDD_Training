b17e3f6c378d57faa7d63f913fde28a3ba3cefd3
Binh Tran <binhtran.vnu@gmail.com>
2013-06-06 13:58:52 +0700

    04 red

--- TDDCalculatorDay06/src/test/java/com/qsoft/tdd/TestCalculator.java
+++ TDDCalculatorDay06/src/test/java/com/qsoft/tdd/TestCalculator.java
@@ -31,6 +31,10 @@ public class TestCalculator {
     public void testAddHandleNewLinesBetweenNumbers(){
         assertEquals(6,Calculator.add("1\n2,3"));
     }
+    @Test
+    public void testAddWithACustomDelimiter(){
+        assertEquals(3,Calculator.add("//;\n1;2"));
+    }
 
 
 
