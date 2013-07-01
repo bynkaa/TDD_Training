c0232f61c85b4ff6f275c9280c78b2738f9a1ab4
Binh Tran <binhtran.vnu@gmail.com>
2013-07-01 13:56:27 +0700

    01 red

--- TDDDaoTestDay01/src/main/java/com/qsoft/tdd/BankAccountDao.java
+++ TDDDaoTestDay01/src/main/java/com/qsoft/tdd/BankAccountDao.java
@@ -1,5 +1,9 @@
 package com.qsoft.tdd;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: BinkaA
@@ -8,6 +12,11 @@ package com.qsoft.tdd;
  * To change this template use File | Settings | File Templates.
  */
 public class BankAccountDao {
+    private Connection dbConnection;
+    public BankAccountDao(DataSource dataSource) throws SQLException {
+        this.dbConnection = dataSource.getConnection();
+    }
+
     public void save(BankAccountDTO bankAccountDTO) {
         //To change body of created methods use File | Settings | File Templates.
     }
--- TDDDaoTestDay01/src/test/java/com/qsoft/tdd/DaoTest/BankAccountDaoTest.java
+++ TDDDaoTestDay01/src/test/java/com/qsoft/tdd/DaoTest/BankAccountDaoTest.java
@@ -1,13 +1,24 @@
 package com.qsoft.tdd.DaoTest;
 
+import com.qsoft.tdd.BankAccountDTO;
+import com.qsoft.tdd.BankAccountDao;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.h2.jdbcx.JdbcDataSource;
 import org.h2.tools.RunScript;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import javax.sql.DataSource;
 import java.nio.charset.Charset;
 import java.sql.SQLException;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,16 +28,40 @@ import java.sql.SQLException;
  * To change this template use File | Settings | File Templates.
  */
 public class BankAccountDaoTest {
-    private final String JDBC_DRIVER = org.h2.Driver.class.getName();
-    private final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-    private final String USER = "sa";
-    private final String PASSWORD = "";
+    private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
+    private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
 //    create db schemal
     @BeforeClass
-    public void createSchema() throws SQLException {
+    public static void createSchema() throws SQLException {
         String schemaFileName = System.class.getResource("/schema.sql").toString();
         System.out.println(schemaFileName);
         RunScript.execute(JDBC_URL, USER, PASSWORD, schemaFileName, Charset.forName("UTF8"), false);
+    }
+    // populate the table with test data
+    @Before
+    public void importDataSet() throws Exception {
+        IDataSet dataSet = readDataSet();
+        cleanlyInsert(dataSet);
+    }
+    private void cleanlyInsert(IDataSet dataSet) throws Exception {
+        IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
+    }
+
+    private IDataSet readDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(System.class.getResource("/dataset.xml"));
+    }
+    //
+    @Test
+    public void testFindByAccountNumber() throws Exception {
+        BankAccountDao bankAccountDao = new BankAccountDao(dataSource());
+        BankAccountDTO b = bankAccountDao.get("1234567890");
+        Assert.assertEquals("1234567890",b.getAccountNumber());
+
     }
 
     private DataSource dataSource(){
